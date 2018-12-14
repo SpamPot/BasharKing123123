e7fd2c997e891c02f9f2a317e7236edbc7289cce
SpamPot <45600277+SpamPot@users.noreply.github.com>
2018-12-14 11:12:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,9 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("هنا اي دي الروم")
+let channel =     client.channels.get("523059521950580746")
 setInterval(function() {
-channel.send(`هنا كلام ال سبام`);
+channel.send(`Spammm`);
 }, 30)
 })
 
